d8c56320b69de3bcde44cb56e872bf0653237f68
Chad Miller <chad.miller.3333@gmail.com>
2025-09-21 13:02:22 -0400

    rm

--- static/js/icot.js
+++ static/js/icot.js
@@ -18,20 +18,6 @@ window.onload = () => {
       click(a)
     }
 
-  const click = (anchor) => {
-    postFrame.src = `posts/${anchor.text}`
-    postFrame.title = anchor.text
-    anchor.classList.add(selectedPostClass)
-    window.location.hash = `#${anchor.text}`
-  }
-
-  const postId = decodeURI(window.location.hash)
-
-  postAnchors.forEach(a => {
-    if (`#${a.text}` === postId) {
-      click(a)
-    }
-
     a.onclick = (event) => {
       event.preventDefault()
       postAnchors.forEach(notA => notA.classList.remove(selectedPostClass))
