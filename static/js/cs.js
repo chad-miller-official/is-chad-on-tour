9684354d7df4b836a3c466a04f5c85ceb1b48329
Chad Miller <chad.miller.3333@gmail.com>
2025-11-30 18:28:51 -0500

    It's done

--- static/js/cs.js
+++ static/js/cs.js
@@ -9,14 +9,24 @@ const VERTICAL_DECALS = [
   'thepresent.jpg',
 ]
 
+function setDayBlockProp(prop, value) {
+  $('day-block').each((_, dayBlock) => $(dayBlock).prop(prop, value))
+}
+
+// Hack to prevent resizing corner decal from altering the width of the CS sheet
+$(window).on('load', () => {
+  const dayBlockWidth = `${$('day-block').width()}px`
+  $('.cs-days').css('grid-template-columns', `${dayBlockWidth} auto ${dayBlockWidth}`)
+})
+
 $(() => {
-  $('.cs-toolbox').draggable({cursor: 'move'})
+  $('#toolbox').draggable({cursor: 'move'})
 
   $('#week').on('input', (event) => {
     let date = dayjs(event.target.value, 'YYYY-Www').subtract(3, 'day')
 
     $('day-block').each((_, dayBlock) => {
-      $(dayBlock).prop('date', date.format('M/D/YY'))
+      $(dayBlock).prop('date', date)
       date = date.add(1, 'day')
     })
   })
@@ -35,18 +45,48 @@ $(() => {
       autoHide: true,
     })
 
-  $('#decal')
-    .on('change', (event) => {
-      cornerDecal.children('img').remove()
-      const newDecal = event.target.value
+  $('#decal').on('change', (event) => {
+    cornerDecal.children('img').remove()
+    const newDecal = event.target.value
 
-      if (newDecal) {
-        cornerDecal.append($('<img>').attr('src', `img/cs-sheet/${newDecal}`)).css('height', 'fit-content')
-        verticalDecal.children('img').remove()
+    if (newDecal) {
+      cornerDecal.append($('<img>').attr('src', `img/cs-sheet/${newDecal}`)).css('height', 'fit-content')
+      verticalDecal.children('img').remove()
 
-        if (VERTICAL_DECALS.includes(newDecal)) {
-          verticalDecal.append($('<img>').attr('src', `img/cs-sheet/vert/${newDecal}`)).css('height', 'fit-content')
-        }
+      if (VERTICAL_DECALS.includes(newDecal)) {
+        verticalDecal.append($('<img>').attr('src', `img/cs-sheet/vert/${newDecal}`)).css('height', 'fit-content')
       }
-    })
+    }
+  })
+
+  $('#tableStyle').on('change', (event) => setDayBlockProp('theme', event.target.value))
+
+  $('#decalBottomRight').on('change', () => {
+    $('.cs-day-thursday').css('grid-area', 'b1')
+    $('.cs-day-friday').css('grid-area', 'b2')
+    $('.cs-day-saturday').css('grid-area', 'b3')
+    $('.cs-day-decal').css('grid-area', 'b4')
+  })
+
+  $('#decalTopRight').on('change', () => {
+    $('.cs-day-decal').css('grid-area', 'b1')
+    $('.cs-day-thursday').css('grid-area', 'b2')
+    $('.cs-day-friday').css('grid-area', 'b3')
+    $('.cs-day-saturday').css('grid-area', 'b4')
+  })
+
+  $('#dateFormat').on('change', (event) => setDayBlockProp('dateFormat', event.target.value))
+  $('#uppercaseHeader').on('change', (event) => setDayBlockProp('uppercaseHeader', event.target.checked))
+  $('#boldHeader').on('change', (event) => setDayBlockProp('boldHeader', event.target.checked))
+
+  $('#grayscale').on('change', (event) => {
+    setDayBlockProp('grayscale', event.target.checked)
+
+    const filter = event.target.checked ? 'grayscale(100%)': 'none';
+
+    cornerDecal.css('filter', filter)
+    verticalDecal.css('filter', filter)
+  })
+
+  $('#dateOffset').on('input', (event) => setDayBlockProp('dateOffset', event.target.value))
 })
--- static/js/modules/cs.js
+++ static/js/modules/cs.js
@@ -10,16 +10,23 @@ export class DayBlock extends LitElement {
       table {
         border: 1px solid black;
         border-collapse: collapse;
-        font-size: larger;
+        width: max-content;
       }
 
       td {
         border: 1px solid black;
+        max-width: 9ch;
+        overflow: hidden;
         padding: 2px;
+        text-wrap-mode: nowrap;
       }
 
       td[contenteditable] {
         font-family: "Caveat", cursive;
+      }
+
+      thead > tr:first-of-type {
+        border: 2px solid black;
       }
 
       thead > tr:first-of-type > td {
@@ -27,28 +34,64 @@ export class DayBlock extends LitElement {
         border-right: none;
       }
 
-      thead > tr:first-of-type > td:last-of-type {
-        padding-left: 1.5ch;
+      /* Black & White theme */
+
+      .bw-field, .bw-day, .bw-header, .bw-footer {
+        background-color: white;
       }
 
-      .day-of-week {
-        font-weight: bold;
-      }
+      /* Classic 1 theme */
 
-      .green {
+      .classic1-field {
         background-color: #c9f4d9;
       }
 
-      .orange {
+      .classic1-day {
+        background-color: #fce43a;
+      }
+
+      .classic1-header {
         background-color: #fad445;
       }
 
-      .spacer {
-        height: 3px;
+      .classic1-footer {
+        background-color: #cdf4f3;
       }
 
-      .yellow {
+      /* Classic 2 theme */
+
+      .classic2-field {
         background-color: #fce43a;
+      }
+
+      .classic2-day {
+        background-color: #c9f4d9;
+      }
+
+      .classic2-header {
+        background-color: #fad445;
+      }
+
+      .classic2-footer {
+        background-color: #cdf4f3;
+      }
+
+      /* Classic 3 theme */
+
+      .classic3-field {
+        background-color: #c9f4d9;
+      }
+
+      .classic3-day {
+        background-color: #fad445;
+      }
+
+      .classic3-header {
+        background-color: #fce43a;
+      }
+
+      .classic3-footer {
+        background-color: #cdf4f3;
       }
     `
   }
@@ -56,33 +99,51 @@ export class DayBlock extends LitElement {
   static get properties() {
     return {
       date: {type: String},
+      dateFormat: {type: String},
+      theme: {type: String},
+      boldHeader: {type: Boolean},
+      uppercaseHeader: {type: Boolean},
+      grayscale: {type: Boolean},
+      dateOffset: {type: Number},
     }
   }
 
   constructor() {
     super()
     this.date = 'DATE'
+    this.dateFormat = 'M/D/YY'
+    this.theme = 'classic1'
+    this.boldHeader = true
+    this.uppercaseHeader = false
+    this.grayscale = false
+    this.dateOffset = 0
   }
 
   render() {
-    const times = ['11AM', '1PM', '3PM', '5PM', '7PM', '9PM', '10PM']
+    const times = ['11AM', '1PM', '3PM', '5PM', '7PM', '9PM', '10PM', 'W2d totals']
+    const date = dayjs(this.date)
+
+    let dayOfWeek = this.dataset.dayOfWeek
+
+    if (this.uppercaseHeader) {
+      dayOfWeek = dayOfWeek.toUpperCase()
+    }
 
     return html`
-      <table>
+      <table style="filter: ${this.grayscale ? 'grayscale(100%)' : 'none'}">
         <colgroup>
-          <col class="green" />
+          <col class="${this.theme}-field" />
           <col />
           <col />
           <col />
           <col />
         </colgroup>
         <thead>
-          <tr class="yellow">
-            <td colspan="2" class="day-of-week">${this.dataset.dayOfWeek}</td>
-            <td colspan="3">${this.date}</td>
+          <tr class="${this.theme}-day">
+            <td colspan="2" style="font-weight: ${this.boldHeader ? 'bold' : 'normal'}">${dayOfWeek}</td>
+            <td colspan="3" style="padding-left: ${this.dateOffset}%">${date.isValid() ? date.format(this.dateFormat) : this.date}</td>
           </tr>
-          <tr class="spacer"></tr>
-          <tr class="orange">
+          <tr class="${this.theme}-header">
             <td>TIME</td>
             <td>SALES</td>
             <td>2hr. sales</td>
@@ -96,7 +157,7 @@ export class DayBlock extends LitElement {
               <td>${time}</td>
               ${[...Array(4)].map(_ => html`<td contenteditable></td>`)}
             </tr>`)}
-          <tr class="green">
+          <tr class="${this.theme}-footer">
             <td>Weather</td>
             <td colspan="4" contenteditable></td>
           </tr>
